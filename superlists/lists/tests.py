a75883813345ee5e2155f2061906505355b886d4
make-process-everyday <838727522@qq.com>
2020-01-31 18:17:56 +0800

    完成单元测试用例编写

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -1,10 +1,22 @@
 from django.urls import resolve
 from django.test import TestCase
+from django.http import HttpRequest
+
 from lists.views import home_page
 # unittest.TestCase 的增强版
 
 # Create your tests here.
 class HomePageTest(TestCase):
     def test_root_url_resolves_to_home_page_view(self):
+        # 功能测试
         found = resolve('/')
         self.assertEqual(found.func, home_page)
+
+    def test_home_page_returns_correct_html(self):
+        # 单元测试
+        request = HttpRequest()
+        response = home_page(request)
+        html = response.content.decode('utf8')
+        self.assertTrue(html.startswith('<html>'))
+        self.assertIn('<title>To-Do lists</title>', html)
+        self.assertTrue(html.endswith('</html>'))
--- superlists/lists/views.py
+++ superlists/lists/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # 在这里编写视图
-def home_page():
-    pass
+def home_page(request):
+    # return HttpResponse('<html><title>To-Do lists</title></html>')
+    # render 函数， 根据请求， 调用模板，返回HeepResponse 对象
+    # 模板功能是Django 很强的一个功能
+    return render(request, "home.html")
+
 # Create your views here.
