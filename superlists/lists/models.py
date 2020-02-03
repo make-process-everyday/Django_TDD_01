6970a86d4aa55c7d876d6c7c4ef2ef5b308105ff
make-process-everyday <838727522@qq.com>
2020-02-03 11:45:51 +0800

    修改view 函数的错误，修改功能测试支持两个待办事项的情况。

--- superlists/lists/models.py
+++ superlists/lists/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Item(models.Model):
+    # models.Model 的类映射到数据库中的一个表。
+    # 默认情况下，这种类会得到一个自动生成的 id 属性，作为表的主键;
+    text = models.TextField(default='')
+
